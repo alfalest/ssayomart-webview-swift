b460093aafe9872488253478c624263416b4fbe7
Adib Alfaini Afifi <45699661+alfalest@users.noreply.github.com>
2023-08-04 13:59:48 +0700

    Initial Commit

---
+++ ssayomartapp/ssayomartappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssayomartappApp.swift
+//  ssayomartapp
+//
+//  Created by ANEKA DASUIB JAYA on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ssayomartappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
